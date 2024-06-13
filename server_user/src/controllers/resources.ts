620da16472e4cfeeac5f461130340fdb5b6a1565
sanskritii-1 <nsanskriti03@gmail.com>
2024-06-13 23:45:25 +0530

    fixed some status codes

--- server_user/src/controllers/resources.ts
+++ server_user/src/controllers/resources.ts
@@ -11,7 +11,7 @@ interface CustomRequest extends Request{
 export const getResources = async (req: CustomRequest, res: Response):Promise<Response> => {
     try {
         // await Resource.insertMany(images);
-        if(!req.id) return res.status(400).json({error: "Unauthorised access to resources"});
+        if(!req.id) return res.status(401).json({error: "Unauthorised access to resources: Token not found"});
         
         const resources = await Resource.find();
         return res.status(200).json(resources);
@@ -27,19 +27,19 @@ export const accessResource = async (req: CustomRequest, res: Response):Promise<
     try {
         console.log('payload for resource access: ', req.id);
         if(!req.id){
-            return res.status(400).json({error: "Unauthorised access to resources"});
+            return res.status(401).json({error: "Unauthorised access to resources: Token not found"});
         }
 
         const userId = <JwtPayload>req.id;
         
         const foundUser = await UserResource.findOne({userId: userId.id});
 
-        if(!foundUser) return res.status(500).json({error: "User not found"});
+        if(!foundUser) return res.status(500).json({error: "User resource record not found"});
 
         if(foundUser.leftResources == 0){
-            return res.json(400).json({error: "Can't access anymore resources"});
+            return res.json(403).json({error: "Cannot access anymore resources"});
         }
-        
+
         foundUser.updateOne({userId: userId}, {$inc: {count:-1}});
         return res.status(200).json({msg: "Successfully accessed resource"});
     } 
