f88ab5e6b5f29f7a3df797f9f4ff387b3e665053
Palak281202 <141211678+Palak281202@users.noreply.github.com>
2024-06-19 14:34:15 +0530

    Subscription Action in UI

--- client/src/components/Subscriptions/Subscription.tsx
+++ client/src/components/Subscriptions/Subscription.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./Subscription.css";
 import { useNavigate } from "react-router-dom";
+import { sendData } from "../../helper/util";
 
 const Subscriptions: React.FC = () => {
   const [subscriptions, setSubscriptions] = useState<any[]>([]);
   const navigate = useNavigate();
-
   useEffect(() => {
     const fetchSubscriptions = async () => {
       try {
@@ -23,8 +23,15 @@ const Subscriptions: React.FC = () => {
 
     fetchSubscriptions();
   }, []);
-  const subcribeHandler = () => {
-    navigate("/resources");
+  const subcribeHandler = async () => {
+    try {
+      const resData = await sendData("POST", "/subscribe", true, {
+        subscriptions,
+      });
+    } catch (error) {
+      alert(error);
+      navigate("/login");
+    }
   };
   return (
     <div className="show-subscriptions">
@@ -37,7 +44,9 @@ const Subscriptions: React.FC = () => {
           <p>Duration: {subscription.duration} months</p>
           {/* <p>Features: {subscription.features}</p> */}
           <br />
-          <button onClick={subcribeHandler} className="subscribe-button">Subscribe</button>
+          <button onClick={subcribeHandler} className="subscribe-button">
+            Subscribe
+          </button>
         </div>
       ))}
     </div>
