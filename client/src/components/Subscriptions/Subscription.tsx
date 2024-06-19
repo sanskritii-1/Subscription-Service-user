a8d5cc2102810e07a76b9a28029a1816a4b64bed
Pranjal-sh10 <9921103226@mail.jiit.ac.in>
2024-06-20 00:19:45 +0530

    css+tsx+client/public/index.html

--- client/src/components/Subscriptions/Subscription.tsx
+++ client/src/components/Subscriptions/Subscription.tsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import './Subscription.css';
+import { sendData } from '../../helper/util';
 
 const Subscriptions: React.FC = () => {
   const [subscriptions, setSubscriptions] = useState<any[]>([]);
 
-useEffect(() => {
+  useEffect(() => {
     const fetchSubscriptions = async () => {
       try {
-        const response = await fetch('http://localhost:5000/api/subscription-plans');
-        const data = await response.json();
+        const data = await sendData('GET', 'subscription-plans', false);
         if (Array.isArray(data)) {
-        setSubscriptions(data);
-      }
-      }  catch (error) {
+          setSubscriptions(data);
+        }
+      } catch (error) {
         console.error('Error fetching subscriptions:', error);
       }
     };
@@ -21,20 +21,40 @@ useEffect(() => {
   }, []);
 
   return (
-    <div className='show-subscriptions'>
-      <h2>List of Subscriptions :</h2>
-      {subscriptions.map((subscription) => (
-        <div key={subscription._id}>
-          <h3>{subscription.name}</h3>
-          <p>Resources: {subscription.resources}</p>
-          <p>Price: ${subscription.price}</p>
-          <p>Duration: {subscription.duration} months</p>
-          {/* <p>Features: {subscription.features}</p> */}
-          <br/>
-        </div>
-      ))}
+    <div className='subscriptions-container'>
+      <h2>Available Subscriptions :</h2>
+      <div className='cards'>
+        {subscriptions.map((subscription) => (
+          <div key={subscription._id} className='card'>
+            <div className="card-header">
+              <h3>{subscription.name}</h3>
+              <p className="card-price">${subscription.price} USD</p>
+            </div>
+            <div className="card-body">
+              <div className="card-detail">
+                <i className="fas fa-clock"></i>
+                <span>{subscription.duration} months</span>
+              </div>
+              <div className="card-detail">
+                <i className="fas fa-database"></i>
+                <span>{subscription.resources} Resource Access</span>
+              </div>
+              <div className="card-detail">
+                <i className="fas fa-list"></i>
+                <span>{subscription.features}</span>
+              </div>
+            </div>
+            <button className="card-button">Subscribe now</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default Subscriptions;
+
+
+
+
+
